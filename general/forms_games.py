ba3ac323ce37a18f70bc80b0166da05f070462ad
PanVidla <vilem.obratil@gmail.com>
2023-03-22 19:24:41 +0100

    Added functionality for GameSeries.

--- general/forms_games.py
+++ general/forms_games.py
@@ -2,13 +2,51 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SelectField, DateField, SubmitField
 from wtforms.validators import DataRequired, Optional
 
+from general.models.misc import Company
+
+
+class GameForm(FlaskForm):
+
+    # General
+    name_full = StringField("Full name", validators=[DataRequired()])
+    name_display = StringField("Display name", validators=[DataRequired()])
+    name_short = StringField("Short name", validators=[DataRequired()])
+
+    developer_id = SelectField("Developed by", coerce=int)
+    game_series_id = SelectField("Part of series", coerce=int)
+
+    # Initialization
+    def __init__(self, *args, **kwargs):
+        super(GameForm, self).__init__(*args, **kwargs)
+
+        self.developer_id.choices = [(company.id, "{}".format(company.name_display))
+                                      for company
+                                      in Company.query
+                                     .filter(Company.is_game_developer == True)
+                                     .order_by(Company.name_display.asc()).all()]
+
+
+class GameSeriesForm(FlaskForm):
+
+    # General
+    name = StringField("Name", validators=[DataRequired()])
+
+
+class GameSeriesAddForm(GameSeriesForm):
+
+    submit = SubmitField("Add game series")
+
+
+class GameSeriesEditForm(GameSeriesForm):
+    submit = SubmitField("Edit game series")
+
 
 class PlatformForm(FlaskForm):
 
     # General
     name_full = StringField("Full name", validators=[DataRequired()])
     name_display = StringField("Display name", validators=[DataRequired()])
-    name_short = StringField("Short name", validators=[Optional()])
+    name_short = StringField("Short name", validators=[DataRequired()])
 
 
 class PlatformAddForm(PlatformForm):
--- general/routes_games.py
+++ general/routes_games.py
@@ -2,8 +2,8 @@
 from flask import render_template, flash, redirect, url_for
 
 from general import cardb, database
-from general.forms_games import PlatformAddForm, PlatformEditForm
-from general.models.game import Game, Platform
+from general.forms_games import PlatformAddForm, PlatformEditForm, GameSeriesAddForm, GameSeriesEditForm
+from general.models.game import Game, Platform, GameSeries
 
 
 @cardb.route("/games/", methods=['GET'])
@@ -19,6 +19,18 @@ def overview_games():
                            viewing="games")
 
 
+@cardb.route("/games/game-series", methods=['GET'])
+def overview_game_series():
+
+    game_series = GameSeries.query.order_by(GameSeries.name.asc()).all()
+
+    return render_template("games_overview_game_series.html",
+                           title="All game series",
+                           heading="All game series",
+                           game_series=game_series,
+                           viewing="game_series")
+
+
 @cardb.route("/games/platforms", methods=['GET'])
 def overview_platforms():
 
@@ -29,6 +41,34 @@ def overview_platforms():
                            heading="Platforms",
                            platforms=platforms,
                            viewing="platforms")
+
+
+# Add game series
+@cardb.route("/games/game-series/add-game-series", methods=['GET', 'POST'])
+def add_game_series():
+
+    form = GameSeriesAddForm()
+
+    if form.validate_on_submit():
+
+        new_game_series = GameSeries()
+        form.populate_obj(new_game_series)
+
+        try:
+            database.session.add(new_game_series)
+            database.session.commit()
+        except RuntimeError:
+            flash("There was a problem adding a new game series to the database.", "danger")
+            return redirect(url_for("add_game_series"))
+
+        flash("{} has been successfully added to the database.".format(new_game_series.name), "success")
+        return redirect(url_for("overview_game_series"))
+
+    return render_template("games_form_game_series.html",
+                           title="Add game series",
+                           heading="Add game series",
+                           form=form,
+                           viewing="game_series")
 
 
 # Add platform
@@ -62,7 +102,34 @@ def add_platform():
                            viewing="platforms")
 
 
-# Edit company
+# Edit game series
+@cardb.route("/games/game-series/edit-game-series/<id>", methods=['GET', 'POST'])
+def edit_game_series(id):
+
+    game_series = GameSeries.query.get(id)
+    form = GameSeriesEditForm(obj=game_series)
+
+    if form.validate_on_submit():
+
+        form.populate_obj(game_series)
+
+        try:
+            database.session.commit()
+        except RuntimeError:
+            flash("There was a problem editing {}.".format(game_series.name), "danger")
+            return redirect(url_for("edit_game_series", id=game_series.id))
+
+        flash("{} has been successfully edited.".format(game_series.name, "success"))
+        return redirect(url_for("detail_game_series", id=game_series.id))
+
+    return render_template("games_form_game_series.html",
+                           title="Edit game series",
+                           heading="Edit game series",
+                           form=form,
+                           viewing="game_series")
+
+
+# Edit platform
 @cardb.route("/games/platforms/edit-platform/<id>", methods=['GET', 'POST'])
 def edit_platform(id):
 
@@ -91,7 +158,25 @@ def edit_platform(id):
                            viewing="platforms")
 
 
-# Delete company
+# Delete game series
+@cardb.route("/games/game-series/delete-game-series/<id>", methods=['GET', 'POST'])
+def delete_game_series(id):
+
+    game_series = GameSeries.query.get(id)
+
+    try:
+        database.session.delete(game_series)
+        database.session.commit()
+
+    except RuntimeError:
+        flash("There was a problem with deleting {}.".format(game_series.name), "danger")
+        return redirect(url_for("detail_game_series", id=game_series.id))
+
+    flash("{} has been successfully deleted.".format(game_series.name), "success")
+    return redirect(url_for("overview_game_series"))
+
+
+# Delete platform
 @cardb.route("/games/platforms/delete-platform/<id>", methods=['GET', 'POST'])
 def delete_platform(id):
 
@@ -109,6 +194,19 @@ def delete_platform(id):
                                                               platform.name_full,
                                                               platform.name_short), "success")
     return redirect(url_for("overview_platforms"))
+
+
+# Game series detail
+@cardb.route("/games/game-series/detail/<id>", methods=['GET', 'POST'])
+def detail_game_series(id):
+
+    game_series = GameSeries.query.get(id)
+
+    return render_template("games_detail_game_series.html",
+                           title="{}".format(game_series.name),
+                           heading="{}".format(game_series.name),
+                           game_series=game_series,
+                           viewing="game_series")
 
 
 # Platform detail
